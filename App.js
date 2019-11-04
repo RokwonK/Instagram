647818f9135a21378958ce42475b4264d0b0af03
RokwonK <wonrok97@naver.com>
2019-11-04 15:00:43 +0900

    fisrt App

--- App.js
+++ App.js
@@ -3,9 +3,15 @@ import { StyleSheet, Text, View, Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import MainScreen from './Components/MainScreen';
+import HomeTap from './Components/AppTabNavigator/HomeTap';
 
 const AppStackNavigator = createStackNavigator({
-  Main: { screen: MainScreen }
-});
+  Main: { screen: MainScreen },
+  HomeTap: { screen: HomeTap }
+},
+{
+  initialRouteName: 'Main'
+}
+);
 
 export default createAppContainer(AppStackNavigator);
